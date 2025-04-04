14a32e633a2535969a455d3e4b5a02ef4a34c537
Gillian Punzal <PunzalGillian>
2025-04-04 20:29:41 +0800

    fixed bug in signup

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,4 +1,5 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Routes, Route, useLocation } from "react-router-dom";
+// ... rest of the code remains the same
 
 //pages
 import SignUpPage from "./pages/SignUpPage";
@@ -14,24 +15,24 @@ import Sidebar from "./components/Sidebar";
 import Header from "./components/Header";
 
 function App() {
-  // const user = useUser();
+  const location = useLocation();
+
+  const isAuthRoute =
+    location.pathname === "/signup" || location.pathname === "/login";
 
   return (
     <>
-      <BrowserRouter>
-        <Header />
-        <Sidebar />
-        <Routes>
-          <Route path="/" element={<DashboardPage />} />
-          <Route path="/signup" element={<SignUpPage />} />
-          <Route path="/budget" element={<BudgetPage />} />
-          <Route path="/expenses" element={<ExpensesPage />} />
-          <Route path="/income" element={<IncomePage />} />
-          <Route path="/investment" element={<InvestmentPage />} />
-
-          <Route path="*" element={<NotFound />} />
-        </Routes>
-      </BrowserRouter>{" "}
+      {!isAuthRoute && <Header />}
+      {!isAuthRoute && <Sidebar />}
+      <Routes>
+        <Route path="/" element={<DashboardPage />} />
+        <Route path="/signup" element={<SignUpPage />} />
+        <Route path="/budget" element={<BudgetPage />} />
+        <Route path="/expenses" element={<ExpensesPage />} />
+        <Route path="/income" element={<IncomePage />} />
+        <Route path="/investment" element={<InvestmentPage />} />
+        <Route path="*" element={<NotFound />} />
+      </Routes>
     </>
   );
 }
