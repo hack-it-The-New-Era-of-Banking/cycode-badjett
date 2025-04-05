f356542fc278f7b5cb41f2bbb0580b0324b8e534
Gillian Punzal <PunzalGillian>
2025-04-05 11:09:07 +0800

    added ai insights

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,7 +1,10 @@
+// import { logonobg } from "../assets/icons/logonobg.png";
+
 const Header = (props) => {
   return (
-    <div className=" mb-4 md:ml-64">
-      <h1 className="">Logo here</h1>
+    <div className="mb-4 md:ml-64">
+      {/* <img className="w-12 h-12" src={logonobg} alt="Logo" /> */}
+      <h1 className="">Budget Wingman</h1>
     </div>
   );
 };
